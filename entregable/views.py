0573685dc5ac88790daede06ad419915eb5e4733
SaulGonzalez19 <saul.gonzalez19.06@gmail.com>
2022-10-11 22:52:57 -0300

    Agregar variable {{nombre}} a saludar.html

--- entregable/views.py
+++ entregable/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def saludar(request):
-    return render(request, "entregable/saludar.html")
+    return render(request, "entregable/saludar.html", {"nombre":"Saúl"})
