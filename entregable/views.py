2db5894d1b69ad2569bd731479184803ad6200e1
SaulGonzalez19 <saul.gonzalez19.06@gmail.com>
2022-10-13 21:37:27 -0300

    Visualización de lista de familiares habilitada

--- entregable/views.py
+++ entregable/views.py
@@ -3,15 +3,11 @@ from entregable.models import Familiar
 
 def mostrar_familiares(request):
   lista_familiares = Familiar.objects.all()
-  return render(request, "ejemplo/familiares.html", {"lista_familiares": lista_familiares})
+  return render(request, "entregable/familiares.html", {"lista_familiares": lista_familiares})
 
+def saludar(request):
+    return render(request, "entregable/saludar.html", {"nombre":"Saúl"})
 
-
-
-
-# def saludar(request):
-#     return render(request, "entregable/saludar.html", {"nombre":"Saúl"})
-
-# def imc(request, peso, altura):
-#     imc = peso / (altura*100)**2
-#     return render(request, "entregable/imc.html", {"imc": imc})
+def imc(request, peso, altura):
+    imc = peso / (altura*100)**2
+    return render(request, "entregable/imc.html", {"imc": imc})
